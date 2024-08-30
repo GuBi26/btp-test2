f666e75f97fc9ac3a4ecb27cc938deeaa89e2de6
GuBi26 <149089660+GuBi26@users.noreply.github.com>
2024-08-30 14:17:23 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,29 @@
+// Navbar
+
+const navbar = document.getElementsByTagName("nav")[0];
+
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY > 1) {
+    navbar.classList.replace("bg-transparent", "nav-color");
+  } else if (this.window.scrollY === 0) {
+    navbar.classList.replace("nav-color", "bg-transparent");
+  }
+});
+
+// Carrousel
+
+const carrousel = document.querySelector(".korosel");
+
+const prevButton = document.querySelector(".button-arrow-left");
+const nextButton = document.querySelector(".button-arrow-right");
+
+nextButton.addEventListener("click", function () {
+  carrousel.style.transform = "translateX(-1400px)";
+  console.log(carrousel.scrollLeft);
+});
+
+prevButton.addEventListener("click", function () {
+  carrousel.style.transform = "translateX(-70px)";
+});
+
+
